0963f3d3465f991ea2e50bf75bbdfab370175acb
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-23 21:59:48 -0700

    Add files via upload

--- Combination Sum.py
+++ Combination Sum.py
@@ -23,3 +23,17 @@ class Solution:
             
         dfs(0, [], 0)
         return res
+
+        # Solution 2
+        ret = []
+        self.dfs(candidates, target, [], ret)
+        return ret
+    
+    def dfs(self, nums, target, path, ret):
+        if target < 0:
+            return 
+        if target == 0:
+            ret.append(path)
+            return 
+        for i in range(len(nums)):
+            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
